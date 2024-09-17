16e6f72b9c9ece008dbd41d1b4f04f6736a69af1
Shivam Thorat <shivamthorat00@gmail.com>
2024-09-17 08:50:11 +0530

    new file uploaded

---
+++ lib/pages/signin_page.dart
@@ -0,0 +1,343 @@
+import 'dart:async'; // For Timer
+import 'package:flutter/material.dart';
+import 'consumer_dash_page.dart';
+import 'dashboard_page.dart';
+import 'farmer_dash_page.dart';
+import 'signup_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+// Uncomment or define this if you have the DashboardPage widget somewhere else in your project
+// import 'dashboard_page.dart';
+
+class SigninPage extends StatefulWidget {
+  final String? mobileNumber; // Accept mobile number
+
+  const SigninPage({super.key, this.mobileNumber}); // Constructor
+
+  @override
+  _SigninPageState createState() => _SigninPageState();
+}
+
+class _SigninPageState extends State<SigninPage> {
+  bool otpVisible = false; // Controls OTP field and Sign-in button visibility
+  bool mobileDisabled = false; // Disables mobile input and SEND OTP button
+  final TextEditingController mobileController = TextEditingController(); // Mobile input controller
+  final TextEditingController otpController = TextEditingController(); // OTP input controller
+  String errorMessage = ''; // Displays invalid mobile number error
+  bool resendEnabled = false; // Controls resend button state
+  int secondsRemaining = 59; // Timer countdown for resend button
+  Timer? countdownTimer;
+  bool otpSent = false; // New flag to track OTP sent status
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.mobileNumber != null) {
+      mobileController.text = widget.mobileNumber!;
+    }
+  }
+
+  @override
+  void dispose() {
+    mobileController.dispose();
+    otpController.dispose();
+    countdownTimer?.cancel();
+    super.dispose();
+  }
+
+  void startTimer() {
+    countdownTimer?.cancel(); // Cancel the existing timer if any
+    setState(() {
+      secondsRemaining = 59; // Reset the timer
+      resendEnabled = false;
+    });
+
+    countdownTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (secondsRemaining > 0) {
+          secondsRemaining--;
+        } else {
+          resendEnabled = true;
+          mobileDisabled = false;
+          timer.cancel();
+        }
+      });
+    });
+  }
+
+  void resetForm() {
+    setState(() {
+      otpVisible = false;
+      mobileDisabled = false;
+      errorMessage = '';
+      resendEnabled = false;
+      secondsRemaining = 59;
+      otpSent = false;
+      mobileController.clear();
+    });
+  }
+
+  Future<void> sendOtp(String phoneNumber) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8585/api/login'),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({
+        "mobileno": phoneNumber
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print("OTP Sent");
+      setState(() {
+        otpVisible = true; // Show OTP input field
+        mobileDisabled = true; // Disable mobile input and SEND OTP button
+        startTimer(); // Start the timer for OTP resend
+      });
+    } else {
+      print("Failed to send OTP");
+      setState(() {
+        errorMessage = "Failed to send OTP"; // Display error message
+      });
+    }
+  }
+
+  Future<void> verify(String phoneno, String otp) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8585/api/login/otp-verify'),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({
+        "mobileno": phoneno,
+        "otp": otp,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print("Authenticated");
+      final responseData = jsonDecode(response.body);
+
+      if (responseData['isNewUser'] == true) {
+        // If the user is new, redirect to SignupPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SignupPage(
+              mobileno: phoneno, // Pass the mobile number to SignupPage
+            ),
+          ),
+        );
+      } else {
+        // Fetch user type (Assuming responseData contains 'userType')
+        String userType = responseData['userType']; // 'farmer' or 'consumer'
+
+        if (userType == 'farmer') {
+          // If user is a farmer, navigate to FarmerDashPage
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FarmerDashPage()),
+          );
+        } else if (userType == 'consumer') {
+          // If user is a consumer, navigate to ConsumerDashPage
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ConsumerDashPage()),
+          );
+        } else {
+          setState(() {
+            errorMessage = "Unknown user type"; // Handle unknown user types
+          });
+        }
+      }
+    } else {
+      setState(() {
+        errorMessage = "Invalid OTP"; // Display error message
+      });
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        resetForm();
+        return true;
+      },
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'lib/images/logo.png',
+                height: 50,
+                width: 50,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'HYBRID MARKET',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.green),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'WELCOME',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Sign in to continue',
+                style: TextStyle(fontSize: 16, color: Colors.black54),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                controller: mobileController,
+                decoration: InputDecoration(
+                  labelText: 'Mobile No.',
+                  labelStyle: const TextStyle(color: Colors.black54),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.green),
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.green, width: 2),
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                  errorText: errorMessage.isNotEmpty && !otpVisible ? errorMessage : null,
+                ),
+                keyboardType: TextInputType.phone,
+                enabled: !mobileDisabled,
+              ),
+              const SizedBox(height: 20),
+              AnimatedOpacity(
+                opacity: otpVisible ? 1.0 : 0.0,
+                duration: const Duration(milliseconds: 500),
+                child: otpVisible
+                    ? TextField(
+                  controller: otpController, // Use the OTP controller
+                  decoration: InputDecoration(
+                    labelText: 'OTP',
+                    labelStyle: const TextStyle(color: Colors.black54),
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.green),
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.green, width: 2),
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                    errorText: errorMessage.isNotEmpty && otpVisible ? errorMessage : null,
+                  ),
+                  keyboardType: TextInputType.number,
+                )
+                    : const SizedBox(),
+              ),
+              const SizedBox(height: 20),
+              mobileDisabled
+                  ? ElevatedButton(
+                onPressed: resendEnabled
+                    ? () {
+                   sendOtp(mobileController.text);
+                  // startTimer(); // Restart timer
+                }
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 100),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                ),
+                child: Text(
+                  resendEnabled ? 'RESEND OTP' : 'RESEND OTP ($secondsRemaining)',
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              )
+                  : ElevatedButton(
+                onPressed: () {
+                  String mobileNumber = mobileController.text.trim();
+
+                  if (mobileNumber.length == 10 && RegExp(r'^[0-9]+$').hasMatch(mobileNumber)) {
+                    setState(() {
+                      otpVisible = true;
+                      mobileDisabled = true;
+                      otpSent = true; // Update OTP sent status
+                      errorMessage = '';
+                      startTimer(); // Start countdown timer
+                      sendOtp(mobileController.text);
+                    });
+                  } else {
+                    setState(() {
+                      errorMessage = 'Invalid mobile number';
+                    });
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 120),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                ),
+                child: Text(
+                  otpSent ? 'RESEND OTP' : 'SEND OTP', // Display RESEND OTP if sent
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+              const SizedBox(height: 30),
+              AnimatedOpacity(
+                opacity: otpVisible ? 1.0 : 0.0,
+                duration: const Duration(milliseconds: 500),
+                child: otpVisible
+                    ? ElevatedButton(
+                  onPressed: () {
+                    // if (otpController.text.length == 6 && RegExp(r'^[0-9]+$').hasMatch(otpController.text)) {
+                       verify(mobileController.text, otpController.text);
+                    // } else {
+                    //   setState(() {
+                    //     errorMessage = "Invalid OTP";
+                    //   });
+                    // }
+                  //  String enteredOtp = otpController.text.trim();
+                   //  String mobileNumber = mobileController.text.trim();
+
+ // if (enteredOtp.isNotEmpty) {
+ //   if (mobileNumber == '1234567890') {
+  //    Navigator.push(
+  //      context,
+  //      MaterialPageRoute(builder: (context) => FarmerDashPage()), // Navigate to Farmer Dashboard
+ //     );
+  //  } else {
+ //     Navigator.push(
+  //      context,
+  //      MaterialPageRoute(builder: (context) => ConsumerDashPage()), // Navigate to Consumer Dashboard
+ //     );
+ //   }
+ // } else {
+//    setState(() {
+ //     errorMessage = "Please enter the OTP";
+ //   });
+ // }
+
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 120),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  child: const Text(
+                    'SIGN IN',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                )
+                    : const SizedBox(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/signup_page.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/material.dart';
+import 'signin_page.dart'; // Import the SignInPage
+import 'dashboard_page.dart'; // Import the DashboardPage
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'consumer_dash_page.dart';
+import 'dashboard_page.dart';
+import 'farmer_dash_page.dart';
+
+class SignupPage extends StatefulWidget {
+  final String mobileno;
+  const SignupPage({super.key, required this.mobileno}); // Constructor that accepts mobile number
+
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  String _selectedRole = 'Farmer'; // Default value for the dropdown
+  final TextEditingController nameController = TextEditingController(); // Name input controller
+  final TextEditingController mobileController = TextEditingController(); // Mobile input controller
+  String nameErrorMessage = ''; // Error message for Name field
+  bool isAuthenticated = false; // Flag to check if user is already authenticated
+
+  @override
+  void initState() {
+    super.initState();
+    mobileController.text = widget.mobileno; // Initialize mobile field with the provided number
+    // Here you can add logic to check if the user is already authenticated
+    // For demo purposes, we'll assume the user is not authenticated.
+    // If they are authenticated, you can set the isAuthenticated flag to true.
+  }
+
+  Future<void> register(String name, String type, String phoneNumber) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8585/api/register'),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({
+        "name": name,
+        "type": type,
+        "mobileno": phoneNumber,
+        "kycStatus":"false"
+      }),
+    );
+
+    if (response.statusCode == 201 || response.statusCode == 200) {
+      // Parse the response body
+      final responseBody = jsonDecode(response.body);
+      String role = responseBody['role'];
+
+      print("Registered as $role");
+
+      // Navigate to different dashboard based on the role
+      if (role == 'Farmer') {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => FarmerDashPage()), // Replace with actual Farmer Dashboard Page
+        );
+      } else if (role == 'Consumer') {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ConsumerDashPage()), // Replace with actual Consumer Dashboard Page
+        );
+      }
+    } else {
+      print("Failed to Register with status: ${response.statusCode}");
+      print("Response body: ${response.body}");
+      // Handle failure cases (e.g., show an error message)
+    }
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Logo Image
+            Image.asset(
+              'lib/images/logo.png', // Add your logo image here
+              height: 50,
+              width: 50,
+            ),
+            const SizedBox(height: 20),
+            // Hybrid Market Text
+            const Text(
+              'HYBRID MARKET',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Welcome Text
+            const Text(
+              'WELCOME',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 10),
+            // Sign up to continue Text
+            const Text(
+              'Sign up to continue',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
+              ),
+            ),
+            const SizedBox(height: 30),
+            // Name Input Box
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                labelStyle: const TextStyle(color: Colors.black54), // Set label color here
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.green),
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.green, width: 2),
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                errorText: nameErrorMessage.isNotEmpty ? nameErrorMessage : null, // Show error for Name
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Mobile Number Input Box
+            TextField(
+              controller: mobileController,
+              readOnly: true, // Mobile number is not editable
+              decoration: InputDecoration(
+                labelText: 'Mobile No.',
+                labelStyle: const TextStyle(color: Colors.black54), // Set label color here
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.green),
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.green, width: 2),
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Dropdown List
+            DropdownButtonFormField<String>(
+              value: _selectedRole,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedRole = newValue!;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'I am a...',
+                labelStyle: const TextStyle(color: Colors.black54),
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.green),
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.green, width: 2),
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+              items: const [
+                DropdownMenuItem(value: 'Farmer', child: Text('Farmer')),
+                DropdownMenuItem(value: 'Consumer', child: Text('Consumer')),
+              ],
+            ),
+            const SizedBox(height: 30),
+            // Sign Up Button
+            ElevatedButton(
+              onPressed: () {
+                if (isAuthenticated) {
+                  // If the user is authenticated, navigate to the dashboard directly
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DashboardPage()), // Navigate to DashboardPage
+                  );
+                } else {
+                  // Otherwise, proceed with registration
+                  String name = nameController.text.trim();
+                  if (name.isEmpty) {
+                    setState(() {
+                      nameErrorMessage = 'Name is required';
+                    });
+                  } else {
+                    setState(() {
+                      nameErrorMessage = ''; // Clear error
+                    });
+                    register(name, _selectedRole, widget.mobileno); // Register the user
+                  }
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 120),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+              child: const Text(
+                'SIGN UP',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Already have an account? Sign in link
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Already have an account? ', style: TextStyle(fontSize: 16, color: Colors.black54)),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SigninPage(mobileNumber: widget.mobileno)),
+                    );
+                  },
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
